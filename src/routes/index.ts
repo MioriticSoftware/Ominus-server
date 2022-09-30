bed2bf41835d58eaab97f2b5090981d6514793e9
Daniel Toniuc <dtoniuc@gmail.com>
2022-09-30 20:08:38 +0300

    Add Jest framework for testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import rootRoutes from './root';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+router.use('/', rootRoutes);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,7 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-const port = process.env.PORT;
 
 app.use('/', routes);
 
-app.listen(port, () => {
-    console.log(`Ominus server listening on port: ${port}`);
-});
+export default app;
