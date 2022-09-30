f7f0bf6734a140e5cf378e3d303b5c64dd1c7c02
Daniel Toniuc <dtoniuc@gmail.com>
2022-09-30 16:52:03 +0300

    Setup prettier
    Use prettier for consistent code formatting.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use('/', routes)
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Ominus server listening on port: ${port}`)
-})
+    console.log(`Ominus server listening on port: ${port}`);
+});
